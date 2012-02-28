838c411b5215a78de68baf4f8b404a28c8f820e1
Dennis Linuz <dennismald@gmail.com>
2012-02-28 00:40:59 -0600

    modified:   Bogo-Sort.py

--- Bogo-Sort.py
+++ Bogo-Sort.py
@@ -4,19 +4,38 @@
 # by: Dennis Linuz <dennismald@gmail.com>					#
 # Demonstration of a bogo-sort								#
 #############################################################
+
+#Input your list of numbers here!
+INPUT_ARRAY = ([1,2,5,3,6,8])
+
+import random
+#Shuffle the array items into a random order
 def randomizeList(array):
+	for x in xrange(0,len(array)):
+		randomNumber = int(random.random()*len(array)-1)	
+		if (not(x == randomNumber)):
+			tempVar = array[x]
+			array[x] = array[randomNumber]
+			array[randomNumber] = tempVar
+	return array
 	
-	
+#Check if the array is in order from least to greated
 def isOrdered(array):
 	i = 0
 	while (i < len(array)):
-		print "Looped"
-		if i < len(array)-1:
-			if not (array[i] < array[i+1]):
+		if (i < len(array)-1):
+			if not (array[i] <= array[i+1]):
 				return False
 		i+=1
 	return True
 
 
-testArray = ([1,22,3,4])
-print isOrdered(testArray)
+testArray=INPUT_ARRAY
+count = 0
+while (not isOrdered(testArray)):
+	print "Shuffling: " + str(testArray)
+	testArray = randomizeList(testArray)
+	count += 1
+print ""
+print "Sorted after " + str(count) + " tries."
+print "Sorted: " + str(testArray)
